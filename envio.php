d45985678a74c9e1af41e73c46576499b328a9ba
deeptec <39011728+deeptec@users.noreply.github.com>
2018-05-08 20:22:14 -0500

    Update envio.php

--- envio.php
+++ envio.php
@@ -1,13 +1,13 @@
 <?php
 $destino="deep_tec@hotmail.com";
 
-$tipo_doc = $_POST["tipo_doc"];
-$carrera = $_POST["carrera"];
-$semestre = $_POST["semestre"];
-$materia = $_POST["materia"];
-$unidad = $_POST["unidad"];
-$profesor = $_POST["profesor"];
-$comentarios = $_POST["comentarios"];
+$tipo_doc = $_POST['tipo_doc'];
+$carrera = $_POST['carrera'];
+$semestre = $_POST['semestre'];
+$materia = $_POST['materia'];
+$unidad = $_POST['unidad'];
+$profesor = $_POST['profesor'];
+$comentarios = $_POST['comentarios'];
 
 $contenido = "Tipo de documento: ".$tipo_doc . "\nCarrera: " . $carrera . "\nSemestre: " . $semestre . "\nMateria: " . $materia . "\nUnidad: " . $unidad . "\nProfesor: " . $profesor . "\nComentarios: " . $comentarios; 
 
